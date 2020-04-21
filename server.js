d358ae7ae353e72df91582e635f2928ce8c455b4
Samantha Judd <2.samantha.judd@gmail.com>
2020-04-21 22:15:52 +1200

    Fixes to get things working

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+// server.js
+const express = require('express');
 
-/* view at http://localhost:2020 */
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/views/index.html'));
+// Define Express App
+const app = express();
+const PORT = process.env.PORT || 2020;
+
+// Virtual Path Prefix '/static'
+app.use(express.static('public'))
+
+app.listen(PORT, () => {
+    console.log('Server connected at:', PORT);
 });
-
-//app.use('/public', express.static(path.join(__dirname, 'public'))) // another way of doing this?
-
-app.listen(2020);
